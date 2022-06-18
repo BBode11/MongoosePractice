691de6ce3b303013acc48ae5b5f571b9d53945d7
Brandon Bode <brandonbode25@gmail.com>
2022-06-17 21:01:27 -0400

    Defined schema and initialized a model class

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+import mongoose from "mongoose";
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/movieApp');
 
+const { Schema } = mongoose;
+
+//defined a schema to map to MongoDB collection
+const movieSchema = new Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+//created a model class
+const Movie = mongoose.model("Movie", movieSchema);
